ec415f90d6cd9b6220e8ac99f6005dc734a7fcf9
W0JW0J <95754666+W0JW0J@users.noreply.github.com>
2023-12-20 10:46:04 +0100

    Update project
    Finishing

--- agent.py
+++ agent.py
@@ -3,26 +3,27 @@ import random
 import numpy as np
 from collections import deque  # data structure
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
-BATCH_SIZE = 1000 # grab x samples from memory
+BATCH_SIZE = 1000  # grab x samples from memory
 LR = 0.001
 
 
 class Agent:
 
-    def __int__(self):
+    def __init__(self):
         self.n_games = 0  # number of games
         self.epsilon = 0  # param to control the randomness
-        self.gamma = 0  # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # if we exceed the memory, it will remove elements from the left = popleft()
-        self.model = None  # TODO
-        self.trainer = None  # NONE
+        self.gamma = 0.9  # discount rate, smaller than 1
+        self.memory = deque(maxlen=MAX_MEMORY)  # if we exceed the memory, it will remove elements from the left = popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         # TODO: model, trainer
 
-
     def get_state(self, game):
-        head = game.snake[0] # head is the first item in the list
+        head = game.snake[0]  # head is the first item in the list
         # points next to the head in all directions
         # to check if it hits the boundary
         point_l = Point(head.x - 20, head.y)
@@ -39,7 +40,7 @@ class Agent:
 
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or # going right and collision right
+            (dir_r and game.is_collision(point_r)) or  # going right and collision right
             (dir_l and game.is_collision(point_l)) or
             (dir_u and game.is_collision(point_u)) or
             (dir_d and game.is_collision(point_d)),
@@ -69,14 +70,15 @@ class Agent:
             game.food.y > game.head.y  # food down
         ]
 
-        return np.array(state, dtype=int) # convert state list into numpy array of type int. Trick to convert Boolean to binary
+        return np.array(state,
+                        dtype=int)  # convert state list into numpy array of type int. Trick to convert Boolean to binary
 
     def remember(self, state, action, reward, next_state, done):  # done = current game over
         self.memory.append((state, action, reward, next_state, done))  # pass tuple, popleft if MAX_MEMORY is reached
 
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE) # return list of tuples
+            mini_sample = random.sample(self.memory, BATCH_SIZE)  # return list of tuples
         else:
             mini_sample = self.memory
 
@@ -87,7 +89,7 @@ class Agent:
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
-    def get_action(self, state): # 1:08:30 YT
+    def get_action(self, state):  # 1:08:30 YT
         # random moves: tradeoff exploration (explore the environment) / exploitation (exploit agent/model)
         self.epsilon = 80 - self.n_games
         final_move = [0, 0, 0]
@@ -96,50 +98,55 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
-            move = torch.argmax(prediction).item() # get max
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()  # get max
             final_move[move] = 1
 
         return final_move
 
-    def train(self):
-        plot_scores = [] # used for plotting
-        plot_mean_scores = []
-        total_score = 0
-        record = 0
-        agent = Agent()
-        game = SnakeGameAI()
-        while True:
-            # get old state
-            state_old = agent.get_state(game)
 
-            # get move
-            final_move = agent.get_action(state_old)
+def train():
+    plot_scores = []  # used for plotting
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
 
-            # perform move and get new state
-            reward, done, score = game.play_step(final_move)
-            state_new = agent.get_state(game)
+        # get move
+        final_move = agent.get_action(state_old)
 
-            # train short memory
-            agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
 
-            # remember
-            agent.remember(state_old, final_move, reward, state_new, done)
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
-            if done:
-                # train long memory (experience replay), plot result
-                game.reset()
-                agent.n_games += 1 # increase number of games
-                agent.train_long_memory()
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
 
-                if score > record:
-                    record = score
-                    # agent.model.save()
+        if done:
+            # train long memory (experience replay), plot result
+            game.reset()
+            agent.n_games += 1  # increase number of games
+            agent.train_long_memory()
 
-                print('Game', agent.n_games, 'Score', score, 'Record: ', record)
+            if score > record:
+                record = score
+                agent.model.save()
 
-                # TODO: plot
+            print('Game', agent.n_games, 'Score', score, 'Record: ', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
-    if __name__ == '__main__':
-        train()
+if __name__ == '__main__':
+    train()
